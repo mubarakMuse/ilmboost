0af96bc7982b727b91294c71f5ae4ff7e7b1aa84
mubarakmuse <mubarak014@gmail.com>
2025-12-16 23:07:51 -0600

    updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,12 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://ilmboost.com",
   generateRobotsTxt: true,
   // use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
-  exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*", "/account", "/dashboard"],
+  exclude: [
+    "/twitter-image.*", 
+    "/opengraph-image.*", 
+    "/icon.*", 
+    "/account", 
+    "/dashboard",
+    "/admin/*"
+  ],
 };
